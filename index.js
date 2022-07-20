327ad84acdab678da0d428ff2f6bfbcf6dfd108b
Matt Dittes <mattdittes@gmail.com>
2022-07-20 15:40:52 -0400

    commit tree eac6ab6ef014dedf6b3566eac4a32b226498e976

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const goodbye = document.getElementById("main");
+goodbye.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.textContent = "Matt is the champion";
